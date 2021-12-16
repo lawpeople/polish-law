89a191d6d7d2dd9c056e0de7d5d3494056358963
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2021-12-16 22:36:14 +0100

    Update LegalAct dataclass definition

--- src/liblegis/legal_act.py
+++ src/liblegis/legal_act.py
@@ -7,6 +7,10 @@ class LegalAct:
     volume: int
     position: int
     title: str
+    promulgation_date: str | None
+    announcement_date: str | None
+    comes_in_force_date: str | None
+    effective_date: str | None
     content: str | None
 
     @classmethod
