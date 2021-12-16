36af4079a8608a7b3abdab13c5869767478e2c99
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2021-12-16 17:50:47 +0100

    Add more of the required pygit2 typing stubs

--- typings/pygit2/__init__.py
+++ typings/pygit2/__init__.py
@@ -2,6 +2,11 @@ from __future__ import annotations
 
 from typing import Iterator
 
+GIT_SORT_NONE = 0
+GIT_SORT_TOPOLOGICAL = 1
+GIT_SORT_TIME = 2
+GIT_SORT_REVERSE = 4
+
 
 class Repository:
     head: Reference
@@ -9,7 +14,7 @@ class Repository:
     def __init__(self, path: str) -> None:
         ...
 
-    def walk(self, oid: Oid) -> Iterator[Commit]:
+    def walk(self, oid: Oid, sort_mode: int = GIT_SORT_NONE) -> Iterator[Commit]:
         ...
 
     def revparse_single(self, revision: str) -> Commit:
