14fa098d2269c55af5c16f1aeb2049141fd58e04
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2021-12-20 00:37:03 +0100

    Remove unused method stubs from LegalAct dataclass

--- src/liblegis/legal_act.py
+++ src/liblegis/legal_act.py
@@ -12,11 +12,3 @@ class LegalAct:
     comes_in_force_date: str | None
     effective_date: str | None
     content: str | None
-
-    @classmethod
-    def from_year_and_pos(cls, year: int, position: int):
-        ...
-
-    @classmethod
-    def from_filename(cls):
-        ...
